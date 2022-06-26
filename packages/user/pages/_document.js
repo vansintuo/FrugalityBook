d0bd9f418538e683ad9db67d9ea410c555f482f9
Tykea <Tykeaboyloy@gmail.com>
2022-06-26 13:25:20 +0700

    change font to lato

--- packages/user/pages/_document.js
+++ packages/user/pages/_document.js
@@ -17,6 +17,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Khmer&display=swap"
             rel="stylesheet"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
