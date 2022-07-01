5c485b46a18c5e7e2c8631fff84807ab7174b50a
Phirun_Oem <phirunoemark123@gmail.com>
2022-07-01 20:13:49 +0700

    config package , _document and gitignore

--- packages/user/pages/_document.js
+++ packages/user/pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
             rel="stylesheet"
