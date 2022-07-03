a6b224fe55a23f6f9c581a6e803371ada2c497b2
Phirun_Oem <phirunoemark123@gmail.com>
2022-07-03 16:13:51 +0700

    fix 1000+

--- packages/user/pages/index.js
+++ packages/user/pages/index.js
@@ -13,6 +13,8 @@ import { userState } from "../states/AuthGuardState/userState";
 import { useRecoilState } from "recoil";
 import BookUnder5 from "../components/containers/layouts/BookUnder5";
 import BestSeller from "../components/containers/layouts/BestSeller";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const useStyle = makeStyles({
   bigContainer: {
@@ -42,10 +44,10 @@ const useStyle = makeStyles({
       // marginTop: "10px",
     },
   },
-  Header:{
-    marginTop:"30px"
+  Header: {
+    marginTop: "30px",
   },
-  textArrival:{ 
+  textArrival: {
     marginTop: "45px",
     height: "100px",
     position: "relative",
@@ -105,13 +107,15 @@ export async function getServerSideProps(ctx) {
   };
 }
 const Home = ({ user, dataProps }) => {
+  const theme = useTheme();
+  const Match = useMediaQuery(theme.breakpoints.down("md"));
   const [data, setData] = useState([]);
   const classes = useStyle();
-  const [userStateValue, setUserStateValue] = useRecoilState(userState)
+  const [userStateValue, setUserStateValue] = useRecoilState(userState);
   if (user) {
-    setUserStateValue(user)
+    setUserStateValue(user);
   }
- 
+
   // ::::::::::::: called function convertPathToURL ::::::::::::::::
   React.useEffect(() => {
     convertPathToURL(dataProps.data).then((res) => {
@@ -122,12 +126,12 @@ const Home = ({ user, dataProps }) => {
     <div className={classes.bigContainer}>
       <Navbar user={user} />
       <div style={{ width: "100%" }}>
-        <NewMainBg user={user}/>
+        <NewMainBg user={user} />
       </div>
       <div className={classes.textMoreBook}>
         <span>1000+</span>
         <h3>BOOKS SELL IN OUR WEBSITE</h3>
-        <Typography style={{ marginTop: "20px",}}>
+        <Typography style={{ marginTop: "20px" }}>
           Our website has alot of books that come from different school that you
           can buy or sell as well.
         </Typography>
@@ -136,45 +140,61 @@ const Home = ({ user, dataProps }) => {
         <Grid
           container
           spacing={3}
-          alignItems= 'center'
-          justifyContent='space-around'
-          direction='row'
+          alignItems="center"
+          justifyContent="space-around"
+          direction="row"
         >
-          <Grid item xs={12} lg={3} sm={6} md={3}>
-            <Paper elevation={0} style={{textAlign:'center'}}><CardWithoutContent 
-            src='https://www.gannett-cdn.com/-mm-/22212976ebafcf5f2c30e55526085cc9f0cae3a2/c=85-0-424-339&r=x233&c=210x230/local/-/media/2017/05/25/Louisville/Louisville/636313252324286257-649149229.jpg'/></Paper>
-          </Grid>
-          <Grid item xs={12} lg={3} sm={6} md={3}>
-            <Paper elevation={0} style={{textAlign:'center'}}><CardWithoutContent 
-            src='https://i0.wp.com/northforker.com/files/2021/02/8E708912-4A5A-4183-B83D-5D6ED1F42496.jpeg?resize=640%2C633&ssl=1'/></Paper>
-          </Grid>
-          <Grid item xs={12} lg={3} sm={6} md={3}>
-            <Paper elevation={0} style={{textAlign:'center'}}><CardWithoutContent 
-            src='https://neatplaces.co.nz/media/thumbs/uploads/places/place/dead_souls_bookshop/Dead_Souls_Bookshop_Dunedin_9.jpg.650x425_q80_crop-smart_upscale.jpg'/></Paper>
-          </Grid>
-          <Grid item xs={12} lg={3} sm={6} md={3}>
-            <Paper elevation={0} style={{textAlign:'center'}}><CardWithoutContent 
-            src='https://i.pinimg.com/736x/58/f2/f6/58f2f61e1be6b900a5b0ceb32c48a417.jpg'/></Paper>
-          </Grid>
+          {Match ? (
+            <>
+              <Grid item xs={12} lg={3} sm={6} md={3}>
+                <Paper elevation={0} style={{ textAlign: "center" }}>
+                  <CardWithoutContent src="https://www.gannett-cdn.com/-mm-/22212976ebafcf5f2c30e55526085cc9f0cae3a2/c=85-0-424-339&r=x233&c=210x230/local/-/media/2017/05/25/Louisville/Louisville/636313252324286257-649149229.jpg" />
+                </Paper>
+              </Grid>
+            </>
+          ) : (
+            <>
+              <Grid item xs={12} lg={3} sm={6} md={3}>
+                <Paper elevation={0} style={{ textAlign: "center" }}>
+                  <CardWithoutContent src="https://i0.wp.com/northforker.com/files/2021/02/8E708912-4A5A-4183-B83D-5D6ED1F42496.jpeg?resize=640%2C633&ssl=1" />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} lg={3} sm={6} md={3}>
+                <Paper elevation={0} style={{ textAlign: "center" }}>
+                  <CardWithoutContent src="https://neatplaces.co.nz/media/thumbs/uploads/places/place/dead_souls_bookshop/Dead_Souls_Bookshop_Dunedin_9.jpg.650x425_q80_crop-smart_upscale.jpg" />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} lg={3} sm={6} md={3}>
+                <Paper elevation={0} style={{ textAlign: "center" }}>
+                  <CardWithoutContent src="https://i.pinimg.com/736x/58/f2/f6/58f2f61e1be6b900a5b0ceb32c48a417.jpg" />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} lg={3} sm={6} md={3}>
+                <Paper elevation={0} style={{ textAlign: "center" }}>
+                  <CardWithoutContent src="https://i.pinimg.com/736x/58/f2/f6/58f2f61e1be6b900a5b0ceb32c48a417.jpg" />
+                </Paper>
+              </Grid>
+            </>
+          )}
         </Grid>
       </div>
       <div>
         <SomeFact />
       </div>
       <div>
-      <NewArrival data={dataProps}/>
+        <NewArrival data={dataProps} />
       </div>
       <div>
         <HelpDonate />
       </div>
       <div>
-        <BestSeller data={dataProps}/>
+        <BestSeller data={dataProps} />
       </div>
       <div>
         <Testimonail />
       </div>
       <div>
-        <BookUnder5 data={dataProps}/>
+        <BookUnder5 data={dataProps} />
       </div>
     </div>
   );
