1ad0dc1a91e5051801195a48d4a5dd2c95501cac
Phirun_Oem <phirunoemark123@gmail.com>
2022-07-17 17:11:53 +0700

    delete file involve firebase

--- packages/seller/utils/functions/updateData.js
+++ packages/seller/utils/functions/updateData.js
@@ -1,14 +1,15 @@
-import {fireStore} from '../../services/firebase'
-function updateData (collectionName,document,id){
-    console.log('called')
-    fireStore.collection(collectionName)
+import { fireStore } from "../../services/firebase";
+function updateData(collectionName, document, id) {
+  console.log("called");
+  fireStore
+    .collection(collectionName)
     .doc(id)
-    .update(document).then((res)=>{
-        console.log("update success")
-    }).catch((err)=>{
-        console.log(err.message)
+    .update(document)
+    .then((res) => {
+      console.log("update success");
     })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
-export  {
-    updateData,
-}
+export { updateData };
