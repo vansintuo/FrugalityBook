c45b60ea1c72b045b76430f8eabf480d75b6936d
Phirun_Oem <phirunoemark123@gmail.com>
2022-07-20 11:04:29 +0700

    fix server

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,6 +1,6 @@
 // exporting any method from mongo
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 // handle how to connect mongo cluster
 // mongoose.connect("mongodb+srv://phirun:fru123@cluster0.xw2us.mongodb.net/onlineBookstore?retryWrites=true&w=majority")
 // .then(()=>console.log('mongo cluster is connected : :::::::: ')).catch((error)=>console.log('connect mongo cluster is failed', error))
@@ -14,43 +14,43 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 // using socket.io
-const http = require('http')
+const http = require("http");
 const server = http.createServer(app);
-const socketio = require('socket.io')
+const socketio = require("socket.io");
 // log request (see data of user requests)
-app.use(morgan("tiny")); 
+app.use(morgan("tiny"));
 // catch data using body parser
-app.use(bodyParser.urlencoded()); 
- // catch data using file as json
- app.use(express.json());
- // access to file path that store data in env file that it's hidden when we push to bitbucket
- dotenv.config({ path: "config.env" });
- // it is port that store in env file
-const PORT = process.env.PORT; 
+app.use(bodyParser.urlencoded());
+// catch data using file as json
+app.use(express.json());
+// access to file path that store data in env file that it's hidden when we push to bitbucket
+dotenv.config({ path: "config.env" });
+// it is port that store in env file
+const PORT = process.env.PORT;
 //call connection that connect to data base
-const connectionDB= require('./utils/db/connection')
-connectionDB()
+const connectionDB = require("./utils/db/connection");
+connectionDB();
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
 // function socket.io;
 const io = socketio(server, {
   transports: ["polling"],
   cors: {
-    origin: [
-      "http://localhost:3000" ,
-      "http://localhost:3001"
-    ]// we can use array instead of if it has more than one url
-  }
-})
-io.on("connection", (socket)=>{
-  console.log('::::::::::::::::::::::client is connected')
-})
-module.exports = io ;
+    origin: ["http://localhost:3000", "http://localhost:3001"], // we can use array instead of if it has more than one url
+  },
+});
+io.on("connection", (socket) => {
+  console.log("::::::::::::::::::::::client is connected");
+});
+module.exports = io;
 // calling rout from route files
-require('./routers/book.routes')(app)
+require("./routers/book.routes")(app);
 require("./routers/user.routes")(app);
 require("./routers/category.routes")(app);
 require("./routers/checkout.routes")(app);
-require('./routers/status.routes')(app);
+require("./routers/status.routes")(app);
 // listen is used to define port that we run on
 server.listen(PORT, () => {
-  console.log( `server is starting  http://localhost:${PORT}`);
+  console.log(`server is starting  http://localhost:${PORT}`);
 });
