c815afa9471d6d7236c2b79a3805ff15f2568234
Phirun_Oem <phirunoemark123@gmail.com>
2022-07-22 08:17:47 +0700

    change database connection localhost

--- packages/backend/utils/db/connection.js
+++ packages/backend/utils/db/connection.js
@@ -3,7 +3,7 @@ const db = require("./../../models");
 
 const connectionDB = async () => {
   try {
-    await db.mongoose.connect("mongodb://localhost:27017/onlineBookstore");
+    await db.mongoose.connect("mongodb://127.0.0.1:27017/onlineBookstore");
     console.log("**====>> database is connected <<====**");
   } catch (error) {
     throw error;
