f5d726239ac1ccfb65814924e0a545481f451971
Phirun_Oem <phirunoemark123@gmail.com>
2022-07-27 14:17:10 +0700

    change port server

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 // access to file path that store data in env file that it's hidden when we push to bitbucket
 dotenv.config({ path: "config.env" });
 // it is port that store in env file
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 //call connection that connect to data base
 // const connectionDB = require("./utils/db/connection");
 // connectionDB();
